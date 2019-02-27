dd200fcfefbdcd65d9237e488563cb102d66a7e6
MICHAEL PHILOMI <MichaelPhilomi.PaneerSelvam@merrillcorp.com>
2019-02-27 13:10:29 +0530

    Enabling Spring Boot Test

--- src/main/java/com/in28minutes/microservices/limitsservice/controller/LimitsConfigurationController.java
+++ src/main/java/com/in28minutes/microservices/limitsservice/controller/LimitsConfigurationController.java
@@ -3,34 +3,75 @@ package com.in28minutes.microservices.limitsservice.controller;
 import com.in28minutes.microservices.limitsservice.configuration.Configuration;
 import com.in28minutes.microservices.limitsservice.model.LimitsConfiguration;
 
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class LimitsConfigurationController {
+public class LimitsConfigurationController implements InitializingBean,DisposableBean {
 
 
-    @Autowired
-    Configuration configuration;
+	@Autowired
+	Configuration configuration;
 
-    @Value("${minimum}")
-    private int minimum;
 
-    @Value("${maximum}")
-    private int maximum;
+	private int minimum;
 
-    @GetMapping("/limits")
-    public LimitsConfiguration retrieveLimitsFromConfiguration() {
-        // The following line will refer properties from git based file system via Spring cloud config server
-        return new LimitsConfiguration(maximum, minimum);
+	private int maximum;
 
-    }
-    
-    @GetMapping("/limits/via-propety-file")
-    public LimitsConfiguration retrieveLimitsFromPropertyFile() {
-       return new LimitsConfiguration(configuration.getMaximum(),configuration.getMinimum());
+	@GetMapping("/limits")
+	public LimitsConfiguration retrieveLimitsFromConfiguration() {
+		// The following line will refer properties from git based file system via Spring cloud config server
+		return new LimitsConfiguration(maximum, minimum);
 
-    }
+	}
+
+	@GetMapping("/limits/via-propety-file")
+	public LimitsConfiguration retrieveLimitsFromPropertyFile() {
+		return new LimitsConfiguration(configuration.getMaximum(),configuration.getMinimum());
+
+	}
+
+	/**
+	 * @return the minimum
+	 */
+	public int getMinimum() {
+		return minimum;
+	}
+
+	/**
+	 * @param minimum the minimum to set
+	 */
+	@Value("${maximum}")
+	public void setMinimum(int minimum) {
+		this.minimum = minimum;
+	}
+
+	/**
+	 * @return the maximum
+	 */
+	public int getMaximum() {
+		return maximum;
+	}
+
+	/**
+	 * @param maximum the maximum to set
+	 */
+	@Value("${minimum}")
+	public void setMaximum(int maximum) {
+		this.maximum = maximum;
+	}
+
+
+	public void afterPropertiesSet() {
+		System.out.println("The minimum value received :" + maximum);
+		System.out.println("The minimum value received :" + minimum);
+	}
+	
+	public void destroy() {
+		System.out.println("The bean is getting destroyed");
+	   }
 }
